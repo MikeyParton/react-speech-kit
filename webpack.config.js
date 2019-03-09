c0ca7e193fd724d3485014b3be93054d50d2f47d
Michael Parton <mikeparton@live.com.au>
2019-03-09 14:49:33 +1100

    Add github pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.join(__dirname, 'examples/src/index.js'),
+  output: {
+    path: path.join(__dirname, 'examples/dist'),
+    filename: 'bundle.js'
+  },
   module: {
     rules: [
       {
